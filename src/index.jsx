10ff10c2752a75112a866b9d84512d3e00272bd0
Alecia Hagen <alecia.h98@gmail.com>
2025-06-06 01:02:17 -0500

    added Chakra to the project

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+// import { Provider } from '/components/ui/provider';
+import { Provider } from './components/ui/provider';
 
 import App from './components/App/App';
 import './index.css';
@@ -10,8 +12,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
+    <Provider>
     <HashRouter>
       <App />
     </HashRouter>
+    </Provider>
   </React.StrictMode>
 );
